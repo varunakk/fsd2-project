2fcb99c92d27bd384d3cde3138ad31a3da1fd45f
venkatesh1578 <79895284+venkatesh1578@users.noreply.github.com>
2021-11-11 16:14:32 +0530

    login.js by venky

--- Login.js
+++ Login.js
@@ -1,36 +1,46 @@
-import { useState } from "react"
+import {useState} from "react";
 import axios from "axios";
+import "./Login.css";
 import store from "./store";
 import {useHistory} from "react-router-dom";
-
-
-
 function Login(){
-    const [usrname1,setUsername]=useState("");
-    const [Password1,setPassword]=useState("");
-    const history= useHistory();
+    const[username1,setUsername]= useState("");
+    const[password1,setPassword]= useState("");
+    const history = useHistory();
     const LoginUser=()=>{
-        axios.post("/Login",{username:usrname1,password:Password1 }).then((res)=>{
-            console.log("log success");
-            store.dispatch({type:"loginSuccess" , user:usrname1});
-            history.push("/Main");
-
-
-        }).catch((err)=>{
-            console.log("log fail");
+        
+        axios.post("/login",{username:username1, password:password1}).then((red)=>{
+            store.dispatch({type:"loginSuccess"});
+            history.push("/");
+        }
+        ).catch((err)=>{
             store.dispatch({type:"loginFail"});
+            
+            alert("Invalid username or password");
         })
     };
-    return (
-        <div>
-        <h1> login page </h1>
-        <input type='username' placeholder="enter username" onChange={(e)=>setUsername(e.target.value)} /><br/>
-        <input type='Password' placeholder="enter Password" onChange={(e)=>setPassword(e.target.value)} /><br/>
-        <button onClick={LoginUser}>login</button>
+
+    return(
+        <div class = "center">
+            <h1>Login</h1>
+            <div class ="form">
+                <div class ="text-field">
+                    <input type="username" placeholder="enter username" onChange={(e)=>setUsername(e.target.value)}/><br/>
+                </div>
+                <div class = "text-field">
+                    <input type="password" placeholder="enter password" onChange={(e)=>setPassword(e.target.value)}/><br/>
+                </div>
+            
+                
+                <button class="button" onClick={LoginUser}>Login</button>
+                <div class="signup_link">
+                    Not a member?<a href="signup">Signup</a>
+                </div>
+            </div>
+            
         </div>
-
     )
-
-
 }
+
+
 export default Login;
