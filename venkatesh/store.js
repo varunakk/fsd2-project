f18d853cc4e637027ca86ed91757a2b549276e42
venkatesh1578 <79895284+venkatesh1578@users.noreply.github.com>
2021-11-13 10:33:52 +0530

    Add files via upload

---
+++ venkatesh/store.js
@@ -0,0 +1,6 @@
+import LoginReducer from './reducers/LoginReducer';
+import {createStore} from "redux";
+const store = createStore(LoginReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+export default store;
